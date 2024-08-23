40e76330e5daee19e0056e20d9f10227dc93c7ce
Kayllany04 <kayllany_lima@estudante.sesisenai.org.br>
2024-08-23 17:16:26 -0300

    Criação  do primeiro com react bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Dissauro</h1>
+    <h2>Carnivoros</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dignissimos dicta labore itaque temporibus repellat, odio error, necessitatibus hic, repudiandae corporis ratione deleniti voluptas officia laboriosam incidunt dolores magnam. Magnam! </p>
+
+    </>
+  )
+}
+
+export default App
