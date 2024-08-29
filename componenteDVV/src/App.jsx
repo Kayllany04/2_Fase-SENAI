437c84b678c7a52dd92f5090c18b0926c73d8820
Kayllany04 <kayllany_lima@estudante.sesisenai.org.br>
2024-08-29 17:23:44 -0300

    Add criaçaõi  de componentes e mais umas coisas legais e interessaa

---
+++ componenteDVV/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cabecalho from './componentes/Cabecalho'
+import Links from './componentes/Links'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Cabecalho /> 
+      <Links />
+       
+      </>
+  )
+}
+
+export default App
---
+++ componenteDVV/src/componentes/Cabecalho.jsx
@@ -0,0 +1,15 @@
+import'./Cabecalho.css'
+function Cabecalho() {
+  return (
+
+
+
+    <div className="Cabecalho-container">
+      <h1>treinar dragão</h1>
+
+
+    </div>
+  )
+}
+
+export default Cabecalho
