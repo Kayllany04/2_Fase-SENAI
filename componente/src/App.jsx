c6566d052e19370505ed56a5dbedda96c9b96806
Kayllany04 <kayllany_lima@estudante.sesisenai.org.br>
2024-08-29 17:32:52 -0300

    Add de componetes

---
+++ componente/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Como treinar o seu dragão</h1>
+      <h2>Coisas para facilitar a sua vida</h2>
+      <p>Comando para o emmet da ul: ul▶li*6</p>
+         <ul>
+          <li>Atalhos: alt + z para quebra de linha </li>
+          <li>Atalhos: alt + clique  esquerdo para adicionar novos cursores na tela </li>
+          <li>Atalhos: ctrl + shift + k para excluir uma linha inteira </li>
+          <li>Atalhos: ctrl +; para comentar/descomentar </li>
+          <li>Atalhos: windos+. abre teclado de emojis </li>
+          <li>Atalhos: alt + ↕(ceta pra cima ou para baixo), ser para mover uma liha </li>
+          <li>Atalhos: alt + shift + ↕ copia a linha atual ou bloco selecionado</li>
+          <li>Atalhos:  ctrl + j ou ctrl + ' abre o terminal </li>
+         </ul>
+    <p> para esfregar cursores pela tela (digitar varias coisas ao mesmo tempo): clica a rodinha e esfrega ela pela tela</p>
+
+    <p>Extensões:</p>
+   
+   <ul>
+    <li>ES7 react/ redux/GraphQL/react-natives snippets</li>
+    <li>Auto Rename Tag</li>
+    
+   </ul>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
